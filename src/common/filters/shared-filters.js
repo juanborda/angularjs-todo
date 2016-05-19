7a0e118a17386d0316f7cdd490fac6ab4ff85115
Juan <juampiib@gmail.com>
2016-05-19 16:46:19 -0300

    remove unnecesary filters

--- src/common/filters/shared-filters.js
+++ src/common/filters/shared-filters.js
@@ -1,40 +1,4 @@
 angular.module('app.shared-filters', ['app.shared-helpers'])
-    .filter('htmlToPlaintext', function () {
-        return function (text) {
-            return text ? String(text).replace(/<[^>]+>/gm, '') : '';
-        };
-    })
-    .filter('amParse', [function () {
-        return function (value, format) {
-            return moment(value, format);
-        };
-    }])
-    .filter('dateFormat', [function () {
-        function amDateFormatFilter(value, format) {
-            if (!value && angular.isUndefined(value)) {
-                return '';
-            }
-
-            var date = moment(value);
-            if (!date.isValid()) {
-                return '';
-            }
-
-            return date.format(format);
-        }
-
-        return amDateFormatFilter;
-    }])
-    .filter('asMoney', function () {
-        return function (input) {
-            return '$' + Number(input).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-        };
-    })
-    .filter('asInt', function () {
-        return function (input) {
-            return parseInt(input);
-        };
-    })
     .filter('capitalize', function () {
         return function (input, scope) {
             if (input !== null) {
